e532aa923b9c3fb74a8ecfdd19288053f7d2f72e
Mi4ok <mishik.9.9.3@gmail.com>
2023-10-17 16:22:55 +0300

    Upd

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function(){
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({prefix: "", suffix: ".min"}))
         .pipe(autoprefixer())
