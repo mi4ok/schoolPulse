599ec9510f8ffd591bebd9aca3dc5140f2c4aba8
Mi4ok <mishik.9.9.3@gmail.com>
2023-10-18 17:18:39 +0300

    Каталог, слайдер, описание

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,25 @@ $(document).ready(function(){
             }
         ]
     });
+
+    //ТАБЫ
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(itemClass) {
+      $(itemClass).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__lists').eq(i).toggleClass('catalog-item__lists_active')
+        })
+      });
+    }
+
+    toggleSlide('.catalog-item__link_list');
+    toggleSlide('.catalog-item__link');
 });
